714dbfdba14166a524b00ebb77e9a1050cce0a52
chunk <chunk9@163.com>
2024-10-13 14:38:15 +0800

    stage

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 apt-get install -y build-essential g++ scons automake autoconf m4 perl flex bison byacc
-apt-get install -y libconfig-dev libconfig++-dev libhdf5-dev libelf-dev libxerces-c-dev libboost-all-dev
-
+apt-get install -y libconfig-dev libconfig++-dev libhdf5-dev libelf-dev libxerces-c-dev
+# apt-get install -y libboost-all-dev
 ln -s /usr/include/asm-generic /usr/include/asm
